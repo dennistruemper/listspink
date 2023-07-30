02d214c482b689f478ac250946bcbd030180215b
Dennis Trümper <privat@truemper-nord.de>
2023-07-30 22:47:59 +0200

    feat: show version on settings page

---
+++ src/components/version.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { version } from '$app/environment';
+
+	const versionAsInt = parseInt(version);
+	const showVersion = isNaN(versionAsInt) ? version : `dev-${new Date(versionAsInt).toISOString()}`;
+</script>
+
+Version : {showVersion}
--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import FadeIn from '../../components/animation/fadeIn.svelte';
+	import Version from '../../components/version.svelte';
 	import { appStore } from '../../stores/appStore';
 	import { titleStore } from '../../stores/titleStore';
 	titleStore.set({ title: 'Settings', listChooseMode: false });
@@ -12,4 +13,7 @@
 	{#if $appStore.debugMode === true}
 		<br /><button on:click={appStore.reset}>Reset State</button>
 	{/if}
+	<br />
+	<br />
+	<Version></Version>
 </FadeIn>
