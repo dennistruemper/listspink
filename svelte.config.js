973c064660fa81ceafe27fba0e63b800f8fd5152
dennistruemper <dennis.truemper@gmail.com>
2023-07-30 22:59:44 +0200

    feat: show version on settings page (#32)
    Co-authored-by: Dennis Trümper <privat@truemper-nord.de>

--- svelte.config.js
+++ svelte.config.js
@@ -9,6 +9,9 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
 	},
+	version: {
+		name: '0.0.4'
+	},
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess()
