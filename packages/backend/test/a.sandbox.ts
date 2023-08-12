21887fd8edf26933813bbbd933022a9685bd6a82
Dennis Trümper <privat@truemper-nord.de>
2023-08-12 22:47:47 +0200

    keep going

--- packages/backend/test/a.sandbox.ts
+++ packages/backend/test/a.sandbox.ts
@@ -2,7 +2,7 @@ import { data } from '@ampt/data';
 import { describe, expect, it } from 'vitest';
 describe('a sandbox', () => {
 	it('a sondbox', async () => {
-		data.set('a', 2);
+		data.set('a', 1);
 
 		expect(await data.get('a')).toBe(1);
 	});
