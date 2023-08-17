058c81399e6e11bad9294857dc4bfa513bc11bd1
Dennis Trümper <privat@truemper-nord.de>
2023-08-17 12:58:20 +0200

    fix mail address

--- packages/frontend/test/auth.e2e.ts
+++ packages/frontend/test/auth.e2e.ts
@@ -3,14 +3,17 @@ import { TestGlobalConstants } from './testGlobalConstants';
 
 test('create new user', async ({ page }) => {
 	await page.goto(TestGlobalConstants.getLatestURL());
+	const today = new Date().toISOString().slice(0, 10);
+	const now = today + '-' + Date.now();
+	const mail = `${now}@lists.pink`;
+	const password = 'Test' + today;
 	await page.getByRole('link', { name: 'Account' }).click();
 	await page.getByRole('button', { name: 'Login' }).click();
 	await page.getByRole('link', { name: 'Sign up' }).click();
 	await page.getByLabel('Email address').click();
 
-	const now = new Date().toISOString().slice(0, 10) + '-' + Date.now();
-	await page.getByLabel('Email address').fill(`${now}-lists-pink@roch.digital`);
-	await page.getByLabel('Password').fill('Test20230817');
+	await page.getByLabel('Email address').fill(mail);
+	await page.getByLabel('Password').fill(password);
 	await page.getByRole('button', { name: 'Continue' }).click();
 	await page.getByRole('link', { name: 'Pink List' }).hover();
 	expect(page.url().split('?')[0]).toBe(TestGlobalConstants.getLatestURL());
