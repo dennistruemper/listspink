27e602a93e9ed818850a77cc475460bd462e071c
Dennis Trümper <privat@truemper-nord.de>
2023-10-03 22:06:27 +0200

    get clerk working with elm frontend

---
+++ packages/frontend-elm/src/Pages/Home_.elm
@@ -0,0 +1,105 @@
+module Pages.Home_ exposing (Model, Msg, page)
+
+import Effect exposing (Effect)
+import Html
+import Html.Events exposing (onClick)
+import Page exposing (Page)
+import Route exposing (Route)
+import Shared
+import Shared.Model exposing (User, getUserName)
+import View exposing (View)
+
+
+page : Shared.Model -> Route () -> Page Model Msg
+page shared route =
+    Page.new
+        { init = init shared
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+
+-- INIT
+
+
+type alias Model =
+    { userName : String, user : Maybe Shared.Model.User }
+
+
+init : Shared.Model -> () -> ( Model, Effect Msg )
+init shared () =
+    let
+        userName =
+            case shared.user of
+                Just user ->
+                    getUserName user
+
+                Nothing ->
+                    "please log in"
+    in
+    ( { userName = userName, user = shared.user }
+    , Effect.logging "Pages.Home_.init"
+    )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NoOp
+    | SignInClicked
+    | SignOutClicked
+
+
+update : Msg -> Model -> ( Model, Effect Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model
+            , Effect.none
+            )
+
+        SignInClicked ->
+            ( model
+            , Effect.redirectToSignIn
+            )
+
+        SignOutClicked ->
+            ( model
+            , Effect.signOut
+            )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> View Msg
+view model =
+    let
+        button =
+            case model.user of
+                Just _ ->
+                    Html.button [ onClick SignOutClicked ] [ Html.text "sign out" ]
+
+                Nothing ->
+                    Html.button [ onClick SignInClicked ] [ Html.text "sign in" ]
+    in
+    { title = "Pages.Home_"
+    , body =
+        [ button
+        , Html.text model.userName
+        ]
+    }
---
+++ packages/frontend-elm/src/interop.js
@@ -0,0 +1,98 @@
+import Clerk from '@clerk/clerk-js';
+//import { handleMessageFromElm } from './fromElm';
+let clerk;
+
+// This returns the flags passed into your Elm application
+export const flags = async ({ env }) => {
+	const clerkPublicKey =
+		env.STAGE === 'prod'
+			? 'pk_live_Y2xlcmsubGlzdHMucGluayQ'
+			: 'pk_test_aGFyZHktZ2xpZGVyLTIwLmNsZXJrLmFjY291bnRzLmRldiQ';
+	console.log('Clerk PK', clerkPublicKey);
+	clerk = new Clerk(clerkPublicKey);
+	await clerk.load({});
+	console.log('Clerk is ready: ', clerk.session);
+	const user = clerk.session?.user;
+	const userData = {
+		userName: user?.username,
+		fistName: user?.firstName,
+		lastName: user?.lastName,
+		email: user?.primaryEmailAddress.emailAddress,
+		id: user?.id
+	};
+	console.log('User', userData);
+	const toElm = { user: userData };
+	return toElm;
+};
+
+let websocket;
+// This function is called after your Elm app starts
+export const onReady = ({ app, env }) => {
+	console.log('Elm is ready', app, env);
+	try {
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-ignore
+		websocket = new WebSocket(env.BASE_URL.replace('https', 'wss'));
+	} catch (e) {
+		console.log('Error connecting to Ampt WS', e);
+	}
+	console.log('Connecting to Ampt WS', env.BASE_URL.replace('https', 'wss'));
+
+	console.log('Sending message to elm', app.ports);
+	if (app.ports.toElm.send) {
+		app.ports.toElm.send('Hello to Elm');
+		console.log('Sent message to elm');
+	}
+
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	//@ts-ignore
+	websocket.onmessage = (event) => {
+		console.log('Received message', event.data);
+
+		if (app.ports.toElm.send) app.ports.toElm.send(event.data);
+	};
+
+	if (app.ports.fromElm.subscribe) {
+		app.ports.fromElm.subscribe((data) => {
+			handleMessageFromElm(data, clerk);
+		});
+	}
+};
+
+import { z } from 'zod';
+
+const baseMessageSchema = z.object({
+	tag: z.string(),
+	data: z.string()
+});
+
+export async function handleMessageFromElm(data, clerk) {
+	console.log('Received message', data);
+	const parsed = baseMessageSchema.safeParse(data);
+	if (!parsed.success) {
+		console.log('parsing failed');
+		alert('Invalid message. Please contact support. More information: ' + JSON.stringify(data));
+		console.error(
+			'Invalid message. Please contact support. More information: ' + JSON.stringify(data)
+		);
+		return;
+	}
+
+	switch (parsed.data.tag) {
+		case 'logging':
+			console.log(parsed.data.data);
+			break;
+		case 'signin-redirect':
+			await clerk.redirectToSignIn({});
+			break;
+		case 'signout':
+			clerk.signOut();
+			break;
+		default:
+			alert('Invalid message. Please contact support. More information: ' + JSON.stringify(data));
+			console.error(
+				'Invalid message. Please contact support. More information: ' + JSON.stringify(data)
+			);
+			return;
+	}
+}
