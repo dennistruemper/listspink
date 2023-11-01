141adbd244b233a353e15bc8650b6499757a1011
Dennis Trümper <privat@truemper-nord.de>
2023-11-01 22:28:26 +0100

    skip ampt update

--- scripts/deployAndGetUrl.sh
+++ scripts/deployAndGetUrl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 STAGE=$1
 set +e
-(npx ampt deploy $STAGE || true) |
+(npx ampt --skip-update deploy $STAGE || true) |
 grep -Eo "(https)://[a-zA-Z0-9./?=_%:-]*" > url.txt
 echo $(cat url.txt)
