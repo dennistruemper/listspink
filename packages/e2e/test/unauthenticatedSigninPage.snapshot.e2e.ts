62654d98418a6940f3d567289de03314997e8bf8
Dennis Trümper <privat@truemper-nord.de>
2023-11-05 18:59:17 +0100

    fix test

--- packages/e2e/test/unauthenticatedSigninPage.snapshot.e2e.ts
+++ packages/e2e/test/unauthenticatedSigninPage.snapshot.e2e.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test';
 import { TestGlobalConstants } from './testGlobalConstants';
 
-test('snapshot test of signIn page', async ({ page: Page }) => {
+test('snapshot test of signIn page', async ({ page }) => {
 	await page.goto(TestGlobalConstants.getLatestURL());
 	await page.waitForSelector('text=Unknown User');
 	await expect(page).toHaveScreenshot();
