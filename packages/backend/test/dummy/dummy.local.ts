151976f168098a72963f1d9e654e1ccc748e323d
dennistruemper <dennis.truemper@gmail.com>
2023-11-18 17:45:11 +0100

    chore: use run-pty to start all dev scritps at once (#258)
    * chore: use run-pty to start all dev scritps at once
    
    * do not use node 20 any more
    
    ---------
    
    Co-authored-by: Dennis Trümper <privat@truemper-nord.de>

---
+++ packages/backend/test/dummy/dummy.local.ts
@@ -0,0 +1,13 @@
+import { describe, expect, it } from 'vitest';
+
+// Import the function you want to test
+
+describe('Addition', () => {
+  it('should return the sum of two numbers', () => {
+    // Call the function you want to test
+    const result = 1 + 1;
+
+    // Use the expect function to make assertions
+    expect(result).toBe(2);
+  });
+});
